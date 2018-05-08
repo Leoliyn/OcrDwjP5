e31a2b65b2c16a3617d9bb1ca7ce0e90acd702a1
Leolin1965 <claudeylionel@gmail.com>
2018-05-08 11:29:50 +0200

    modification navbar.php view backend (icone list chapitre et fonction assosciée)

--- view/backend/navbar.php
+++ view/backend/navbar.php
@@ -17,7 +17,14 @@
             <ul class="nav navbar-nav navbar-right">
                 <li><a href='backend-mess.php?action=message'title="Messagerie interne"><i class="fa fa-envelope   fa-3x "></i><span class="badge">5</span></a></li>
                 <li><a href='indexadmin.php?action=changePsswd'title="Changement Password"><i class="fa fa-user   fa-3x "></i><span class="badge"><?php if(isset($_SESSION['user'])){ echo $_SESSION['user'];} ?></span></a></li>
-                <li><a href='indexadmin.php'title ="Gestion des chapitres"><i class="fa fa-file-o   fa-3x "></i></a></li>
+    <?php 
+        if(isset($_GET['ouv_id'])){
+            $ouvId = $_GET['ouv_id'];
+       echo"<li><a href='indexadmin.php?action=listPosts&amp;ouv_id=".$ouvId."'title ='Gestion des chapitres'><i class='fa fa-file-o   fa-3x '></i></a></li>";
+            }
+   
+    ?>
+                
                 <li><a href='indexadmin.php?action=listBooks'title ="Gestion des ouvrages"><i class="fa fa-envira   fa-3x "></i></a></li>
                 <li><a href='index.php'title ="Retour Frontend"><i class="fa fa-home   fa-3x "></i></a></li>
 
